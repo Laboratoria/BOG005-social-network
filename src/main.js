eeec32337c466df56822b217e18feba155d42ce0
SoreyC <soreycortes@gmail.com>
2022-09-02 17:52:09 -0500

    Se crearon carpetas para el proyecto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import  welcome  from "./lib/page/welcome.js";
 
-myFunction();
+document.querySelector("#welcome").innerHTML = welcome();
