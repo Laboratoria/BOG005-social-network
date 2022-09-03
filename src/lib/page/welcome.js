013f9bfd3742208cfa9132c9cb090cbfc0241d81
yaneth-hernandez <yahecaceres@gmail.com>
2022-09-03 14:21:33 -0500

    Cambio sin importancia

--- src/lib/page/welcome.js
+++ src/lib/page/welcome.js
@@ -1,15 +1,15 @@
 const welcome = () => {
-const view = `<h1>Bienvenidos a CICLAPP</h1>
-      <p>¿Te gusta el ciclismo? <br>Esta red social es para ti!
-      </p>
+  const view = `<h1>Bienvenidos a CICLAPP</h1>
+        <p>¿Te gusta el ciclismo? <br>Esta red social es para ti!
+        </p>
 
 
-      <a href="#" id="loginButton" class="loginButton">Registrate</a>
-      <a href="#" id="signIn" class="singIn">Iniciar Sección</a>
-      <a href="#" id="continue" class="continue">Continuar sin registrarse</a>`
+        <a href="#" id="loginButton" class="loginButton">Registrate</a>
+        <a href="#" id="signIn" class="singIn">Iniciar Sección</a>
+        <a href="#" id="continue" class="continue">Continuar sin registrarse</a>`
 
 
-    return view;
+      return view;
 };
 
 export default welcome;
