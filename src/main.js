2625558c435bce47235d5e1388865e91fca19837
KarenthCruz <spoonecologic@gmail.com>
2022-09-06 14:59:21 -0500

    Ruteado Completo

--- src/main.js
+++ src/main.js
@@ -3,44 +3,57 @@
 // import { myFunction } from './lib/index.js';
 // myFunction();
 
+const beginningnContent = '<h1> beginnig opened </h1>';
+const homeContent = '<h1> Home page </h1>'
+const wallContent = '<h1> wall opened </h1>';
+const profileContent = '<h1> profile opened </h1>';
+
+const linkContent = {
+    '#beginning': beginningnContent,
+    '#home': homeContent,
+    '#wall': wallContent,
+    '#profile': profileContent,
+}
+
+
 const root = document.getElementById('root');
 if (window.location.pathname === '/') {
-  root.innerHTML = '<h1> beginnig opened </h1>';
+    root.innerHTML = beginningnContent;
+} else if (window.location.pathname === '/home') {
+    root.innerHTML = homeContent;
 } else if (window.location.pathname === '/wall') {
-  root.innerHTML = '<h1> wall opened </h1>';
+    root.innerHTML = wallContent;
 } else if (window.location.pathname === '/profile') {
-  root.innerHTML = '<h1> profile opened </h1>';
+    root.innerHTML = profileContent;
 }
 
 const changeRoute = (hash) => {
-  if (hash === '#beginning') {
-    window.history.replaceState({}, 'beginning', '/');
-  } else if (hash === '#wall') {
-    window.history.replaceState({}, 'wall', '/wall');
-  } else if (hash === '#profile') {
-    window.history.replaceState({}, 'profile', '/profile');
-  }
+    if (hash === '#beginning') {
+        window.history.replaceState({}, 'beginning', '/');
+    } else if (hash === '#home') {
+        window.history.replaceState({}, 'home', '/home');
+    } else if (hash === '#wall') {
+        window.history.replaceState({},  'wall', '/wall');
+    } else if (hash === '#profile') {
+        window.history.replaceState({}, 'profile', '/profile');
+    }
+
 };
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#beginning') {
-    root.innerHTML = '<h1> beginnig opened </h1>';
-    changeRoute(window.location.hash);
-  } else if (window.location.hash === '#wall') {
-    root.innerHTML = '<h1> wall opened </h1>';
-    changeRoute(window.location.hash);
-  } else if (window.location.hash === '#profile') {
-    root.innerHTML = '<h1> profile opened </h1>';
-    changeRoute(window.location.hash);
-  }
+    const hash = window.location.hash;
+    root.innerHTML = linkContent[hash];
+    changeRoute(hash);
 });
 
 window.onpopstate = () => {
-  if (window.location.pathname === '/') {
-    root.innerHTML = '<h1> beginnig opened </h1>';
-  } else if (window.location.pathname === '/wall') {
-    root.innerHTML = '<h1> wall opened </h1>';
-  } else if (window.location.pathname === '/profile') {
-    root.innerHTML = '<h1> profile opened </h1>';
-  }
+    if (window.location.pathname === '/') {
+        root.innerHTML = beginningnContent;
+    } else if (window.location.pathname === '/wall') {
+        root.innerHTML = homeContent;
+    } else if (window.location.pathname === '/profile') {
+        root.innerHTML = wallContent;
+    } else if (window.location.pathname === '/profile') {
+        root.innerHTML = profileContent;
+    }
 };
