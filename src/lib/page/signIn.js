936ec4d1afa4ca9695f328c718441ff68e291d96
SoreyC <soreycortes@gmail.com>
2022-09-06 09:59:20 -0500

    Se creo html y estilosmobile

--- src/lib/page/signIn.js
+++ src/lib/page/signIn.js
@@ -1,23 +1,41 @@
 const signIn = () => {
-    const viewSignIn = `<form>
+    const viewSignIn = `<section class="viewSignIn">
+    
+        <figure class="logoSignIn">
+        <img src="lib/img/LogoHorizontal.png" alt="LogoCICLAPP" class="logoHorizontal">     
+    </figure>
+
+    <form class="formSignIn">
+<div>
     <label for="name">Nombre y Apellido</label>
     <input type="text" id="name" placeholder="Escribe tu nombre y apellido">
+</div>
 
+<div>
     <label for="user">Usuario</label>
     <input type="text" id="user" placeholder="Escribe tu usuario">
+</div>
 
+<div>
     <label for="email">Correo</label>
     <input type="email" id="email" placeholder="Escribe tu correo">
 
+</div>
+
+<div>
     <label for="password">Contraseña</label>
     <input type="password" id="password" placeholder="Escribe tu contraseña">
+</div>
+   
+    </form> 
 
-    <p>- o -</p>
-    <a href="#">Registrate con Google</a>
+    <p class="o">- o -</p>
+
+    <button type="button" class="googleSignIn"><img src="lib/img/logo_google.svg" alt="GoogleIcon" class="GoogleIcon">Registrate con Google</button>
     
-    <button type="submit">Continuar</button>
-
-    </form>`
+    <button type="submit" class="submitSignIn">Continuar</button>
+    
+</section>`
 
     return viewSignIn;
 };
