3a7a4fa8ce60b852fddc983bdb675771751b136c
yaneth-hernandez <yahecaceres@gmail.com>
2022-09-07 13:23:40 -0500

    Se afinaron detalles de estilo mobile pantalla signIn

--- src/lib/page/signIn.js
+++ src/lib/page/signIn.js
@@ -2,36 +2,40 @@ const signIn = () => {
   const viewSignIn = `<section class="viewSignIn">
     
         <figure class="logoSignIn">
-        <img src="lib/img/LogoHorizontal.png" alt="LogoCICLAPP" class="logoHorizontal">     
+        <img src="lib/img/Logo.png" alt="LogoCICLAPP" class="logoHorizontal"> 
+        <figcaption class="textLogo">CICLAPP</figcaption>
     </figure>
 
     <form class="formSignIn">
-<div>
+<article class="itemsInput">
     <label for="name">Nombre y Apellido</label>
-    <input type="text" id="name" placeholder="Escribe tu nombre y apellido">
-</div>
+    <input type="text" id="name" placeholder="Escribe tu nombre y apellido" value="">
+</article>
 
-<div>
+<article class="itemsInput">
     <label for="user">Usuario</label>
     <input type="text" id="user" placeholder="Escribe tu usuario">
-</div>
+</article>
 
-<div>
+<article class="itemsInput">
     <label for="email">Correo</label>
     <input type="email" id="email" placeholder="Escribe tu correo">
 
-</div>
+</article>
 
-<div>
+<article class="itemsInput">
     <label for="password">Contraseña</label>
     <input type="password" id="password" placeholder="Escribe tu contraseña">
-</div>
+</article>
    
     </form> 
 
-    <p class="o">- o -</p>
+    <p class="o">- O -</p>
 
-    <button type="button" class="googleSignIn"><img src="lib/img/logo_google.svg" alt="GoogleIcon" class="GoogleIcon">Registrate con Google</button>
+    <button type="button" class="googleSignIn">
+    <img src="lib/img/logo_google.svg" alt="GoogleIcon" class="GoogleIcon">
+    <span>Registrate con Google</span>
+    </button>
     
     <button type="submit" class="submitSignIn">Continuar</button>
     
