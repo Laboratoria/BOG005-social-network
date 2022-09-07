5d58a563ca24fd79b80bd15896c992a2d7a02a7a
Alissonred <108753902+Alissonred@users.noreply.github.com>
2022-09-07 08:20:42 -0500

    ajuste

--- src/main.js
+++ src/main.js
@@ -4,56 +4,54 @@
 // myFunction();
 
 const beginningnContent = '<h1> beginnig opened </h1>';
-const homeContent = '<h1> Home page </h1>'
+const homeContent = '<h1> Home page </h1>';
 const wallContent = '<h1> wall opened </h1>';
 const profileContent = '<h1> profile opened </h1>';
 
 const linkContent = {
-    '#beginning': beginningnContent,
-    '#home': homeContent,
-    '#wall': wallContent,
-    '#profile': profileContent,
-}
-
+  '#beginning': beginningnContent,
+  '#home': homeContent,
+  '#wall': wallContent,
+  '#profile': profileContent,
+};
 
 const root = document.getElementById('root');
 if (window.location.pathname === '/') {
-    root.innerHTML = beginningnContent;
+  root.innerHTML = beginningnContent;
 } else if (window.location.pathname === '/home') {
-    root.innerHTML = homeContent;
+  root.innerHTML = homeContent;
 } else if (window.location.pathname === '/wall') {
-    root.innerHTML = wallContent;
+  root.innerHTML = wallContent;
 } else if (window.location.pathname === '/profile') {
-    root.innerHTML = profileContent;
+  root.innerHTML = profileContent;
 }
 
 const changeRoute = (hash) => {
-    if (hash === '#beginning') {
-        window.history.replaceState({}, 'beginning', '/');
-    } else if (hash === '#home') {
-        window.history.replaceState({}, 'home', '/home');
-    } else if (hash === '#wall') {
-        window.history.replaceState({},  'wall', '/wall');
-    } else if (hash === '#profile') {
-        window.history.replaceState({}, 'profile', '/profile');
-    }
-
+  if (hash === '#beginning') {
+    window.history.replaceState({}, 'beginning', '/');
+  } else if (hash === '#home') {
+    window.history.replaceState({}, 'home', '/home');
+  } else if (hash === '#wall') {
+    window.history.replaceState({}, 'wall', '/wall');
+  } else if (hash === '#profile') {
+    window.history.replaceState({}, 'profile', '/profile');
+  }
 };
 
 window.addEventListener('hashchange', () => {
-    const hash = window.location.hash;
-    root.innerHTML = linkContent[hash];
-    changeRoute(hash);
+  const hash = window.location.hash;
+  root.innerHTML = linkContent[hash];
+  changeRoute(hash);
 });
 
 window.onpopstate = () => {
-    if (window.location.pathname === '/') {
-        root.innerHTML = beginningnContent;
-    } else if (window.location.pathname === '/wall') {
-        root.innerHTML = homeContent;
-    } else if (window.location.pathname === '/profile') {
-        root.innerHTML = wallContent;
-    } else if (window.location.pathname === '/profile') {
-        root.innerHTML = profileContent;
-    }
+  if (window.location.pathname === '/') {
+    root.innerHTML = beginningnContent;
+  } else if (window.location.pathname === '/home') {
+    root.innerHTML = homeContent;
+  } else if (window.location.pathname === '/wall') {
+    root.innerHTML = wallContent;
+  } else if (window.location.pathname === '/profile') {
+    root.innerHTML = profileContent;
+  }
 };
