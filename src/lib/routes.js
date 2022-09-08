6301c28660d5436ad35f80cbd5f7c6bc45592248
SoreyC <soreycortes@gmail.com>
2022-09-08 12:56:02 -0500

    Error con evento del botón continuar

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,6 +1,7 @@
 import welcome from './page/welcome.js';
 import header from './templates/header.js';
 
+
 const routes = () => {
   const containerPage = document.getElementById('contentPageId');
   containerPage.innerHTML = header();
