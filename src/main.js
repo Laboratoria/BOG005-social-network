3eada1cffa35142d5b44e7fcd13bfdc0ef575c0b
Alexandra Fuenmayor <91095976+AlexaFuenmayor@users.noreply.github.com>
2022-09-12 10:20:01 -0500

    Revert "cree roteo"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import home from './lib/views/home.js'
-import { changeRoute } from './lib/router.js'
-import { COMPONENTS } from './index.js';
+// Este es el punto de entrada de tu aplicacion
 
-const init = () =>{
-    document.getElementById('content').appendChild(home());
-    changeRoute(window.location.hash, COMPONENTS);
-    window.addEventListener('hashchange', () =>{
-        changeRoute(window.location.hash, COMPONENTS)
-    });
-};
-window.addEventListener('load', init)
+import { myFunction } from './lib/index.js';
+
+myFunction();
