a13fb0ca35e7ff69586a322d23114f6352080c81
KarenthCruz <spoonecologic@gmail.com>
2022-09-12 17:21:57 -0500

    Cambios test

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import {onNavigate} from "../main.js";
+import { onNavigate } from "../main.js";
 
 export const login = () => {
   const div = document.createElement('div');
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,44 @@
 import { register } from '../src/lib/firebase-auth.js';
-import { auth, createUserWithEmailAndPassword  } from '../src/lib/index.js';
+import { auth, createUserWithEmailAndPassword } from '../src/lib/index.js';
 
-jest.mock('../src/lib/index.js', ()=> {
-  return { auth: jest.fn(()=> { //función jest.fn crea una función para jest
-    return { auth: 'TEST'}
+jest.mock('../src/lib/index.js', () => {
+  return {
+    auth: jest.fn(() => { //función jest.fn crea una función para jest
+      return { auth: 'TEST' }
     }),
 
-    createUserWithEmailAndPassword: jest.fn((auth, email, password)=> {
-    if(!email || !password){
-      throw new Error('ERROR')
+    createUserWithEmailAndPassword: jest.fn((auth, email, password) => {
+      if (!email || !password) {
+        throw new Error('ERROR')
       }
       Promise.resolver({ user: 'admin' })
-    }) 
+    })
 
   }
 })
 
-describe('Test for the register function', ()=> {
+
+describe('Test for the register function', () => {
   const email = "admin@test.com";
   const pass = "admin456";
 
-  it('Should call createUserWithEmailAndPassword', async()=> {
-      await register(email, pass)
-      expect(createUserWithEmailAndPassword).toHaveBeenCalled()
+  it('Should call createUserWithEmailAndPassword', async () => {
+    await register(email, pass)
+    expect(createUserWithEmailAndPassword).toHaveBeenCalled()
   })
 
-}
-)
+  it('Should call createUserWithEmailAndPassword with the auth, email, and pass arguments', async () => {
+    await register(email, pass)
+    expect(createUserWithEmailAndPassword).toHaveBeenCalled(auth, email, pass)
+  })
+  it('Should throw an error if executed without arguments', async () => {
+    try {
+      await register()
+    } catch (error) {
+      expect(error).toMatch('ERROR')
+    }
+  })
+})
 
 
 describe('myFunction', () => {
