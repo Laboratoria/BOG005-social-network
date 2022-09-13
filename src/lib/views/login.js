e9e671b0081340d1e211802926953f941ceab341
Teddiechim <107264908+Teddiechim@users.noreply.github.com>
2022-09-13 10:24:38 -0500

    cambios

--- src/lib/views/login.js
+++ src/lib/views/login.js
@@ -1,39 +1,35 @@
-// export default () => {
-//     const LoginSection = document.createElement('div');
-//     LoginSection.style.width='80vw';
-//     LoginSection.style.background="red";
-//     const viewLogin = `
-//     <main>
-//     <section id="contenedor">
-
-//     <article id="contenedorcentrado">
-//         <section id="login">
-//             <form id="loginform">
-//                 <label for="usuario">Usuario</label>
-//                 <input id="usuario" type="text" name="usuario" placeholder="Usuario" required>
-
-//                 <label for="password">Contraseña</label>
-//                 <input id="password" type="password" placeholder="Contraseña" name="password" required>
-
-//                 <button type="submit" title="Ingresar" name="Ingresar">Login</button>
-//             </form>
-
-//         </section>
-//         <div id="derecho">
-//             <div class="titulo">
-//                 Bienvenido
-//             </div>
-//             <hr>
-//             <div class="pie-form">
-//                 <a href="#">¿Perdiste tu contraseña?</a>
-//                 <a href="#">¿No tienes Cuenta? Registrate</a>
-//                 <hr>
-//                 <a href="#">« Volver</a>
-//             </div>
-//         </div>
-//     </article>
-// </section>
-//     </main>`
-//     LoginSection.innerHTML=viewLogin;
-//     return LoginSection;
-// };
+export default () => {
+  const LoginSection = document.createElement("div");
+  LoginSection.style.width = "80vw";
+  LoginSection.style.background = "red";
+  const viewLogin = `
+     <main>
+     <section id="contenedor"
+     <article id="contenedorcentrado">
+         <section id="login">
+             <form id="loginform">
+                 <label for="usuario">Usuario</label>
+                 <input id="usuario" type="text" name="usuario" placeholder="Usuario" required
+                 <label for="password">Contraseña</label>
+                 <input id="password" type="password" placeholder="Contraseña" name="password" required
+                 <button type="submit" title="Ingresar" name="Ingresar">Login</button>
+             </form
+         </section>
+         <div id="derecho">
+             <div class="titulo">
+                 Bienvenido
+             </div>
+             <hr>
+             <div class="pie-form">
+                 <a href="#">¿Perdiste tu contraseña?</a>
+                 <a href="#">¿No tienes Cuenta? Registrate</a>
+                 <hr>
+                 <a href="#">« Volver</a>
+             </div>
+         </div>
+     </article>
+ </section>
+     </main>`;
+  LoginSection.innerHTML = viewLogin;
+  return LoginSection;
+};
--- src/main.js
+++ src/main.js
@@ -1,44 +1,35 @@
 // Motrar el HTML después de seleccionar un link
-// Mostrar el HTML correcto al recargar la página
-
+// Mostrar el HTML correcto al recargar la págin
 const rootDiv = document.getElementById("root");
-
-const homeContent = "<h1>Casita</h1>";
-const aboutContent = "<h1>Acerca de </h1>";
-
+// const homeContent = "<h1>Casita</h1>";
+// const aboutContent = "<h1>Acerca de </h1>"
 // llave => valor
 // home => homeContent
 const linkContent = {
   "#home": homeContent,
   "#about": aboutContent,
 };
-
 const routes = {
   "/": homeContent,
   "/about": aboutContent,
 };
-
 const paths = {
   "#home": "/",
   "#about": "/about",
 };
-
 // Mostrar el HTML correcto al recargar la página
 const pathname = window.location.pathname;
 rootDiv.innerHTML = routes[pathname];
-
 // Cambia la URL para no ocupar el "#"
 const changeRoute = (hash) => {
   window.history.replaceState({}, hash.split("#")[1], paths[hash]);
 };
-
 // Cambiar HTML al clickear los link
 window.addEventListener("hashchange", () => {
   const hash = window.location.hash;
   rootDiv.innerHTML = linkContent[hash];
   changeRoute(hash);
 });
-
 // Muestra el HTML correcto al moverse con los botones de atrás y adelande del navegador
 window.onpopstate = () => {
   const pathname = window.location.pathname;
