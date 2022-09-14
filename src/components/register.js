d1b71f73e6578f3ad2ac9fef77fe702010e6f4aa
Danielasanchez13 <danielasanchezlinares13@gmail.com>
2022-09-14 13:04:55 -0500

    ruteado de login y register

--- src/components/register.js
+++ src/components/register.js
@@ -5,19 +5,18 @@ export const register = () => {
 
       `
       <div class="container">
-      <h1 class="titulo">Explorer</h1>
-      <figure class="imagenDescripcion">
-      <img class="imginInisioSesion" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/travel-%26-tourism-logo-design-template-c613bcb6c381ed8ead4ce8549a79edda_screen.jpg?ts=1599762630">
-      <figcaption>Leyenda de imágen del contenido de la pagina</figcaption>
-      </figure> 
-      <div class="containerDatos"> 
-        <input type="text" name="" id="" class="camapoTexto" placeholder="Nombre">
-        <input type="text" name="" id="" class="camapoTexto" placeholder="Correo">
-        <input type="password" pattern=".{6,}" name="" id="" class="camapoTexto" placeholder="Contraseña">
-        <button class="buttonIniciar"><a href="#">Registrarse</a></button>
-        <p class="registrarse">¿Ya eres miembro?<a href="#">Iniciarsesión</a></p>
-       </div> 
-      </div> 
+	<figure class="imagenDescripcion">
+		<img class="imginInisioSesion" src="IMG/Explore-removebg-preview.png">
+		<figcaption>Leyenda de imágen del contenido de la pagina</figcaption>
+	</figure>
+	<form class="containerDatos">
+		<input type="text" name="" id="" class="camapoTexto" placeholder="Nombre">
+		<input type="text" name="" id="" class="camapoTexto" placeholder="Correo">
+		<input type="password" pattern=".{6,}" name="" id="" class="camapoTexto" placeholder="Contraseña">
+		<button class="buttonIniciar"><a href="#">Registrarse</a></button>
+		<p class="registrarse">¿Ya eres miembrooooo?<a href="#">Iniciarsesión</a></p>
+	</form>
+     </div>
        `
        
           return registrar; 
