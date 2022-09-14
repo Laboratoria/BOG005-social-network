3e0eb3caddcc1ff54edd6ad0c24b72a305d0ca8d
Monik586 <monik.rodriguez586@gmail.com>
2022-09-14 17:32:14 -0500

    Se renderiza el primer botón con appendChild

---
+++ src/components/register.js
@@ -0,0 +1,5 @@
+// export const register= () =>{
+//     const checkIn = document.createElement('h1');
+// };
+
+// checkIn.textContent='Registrate';
--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-const routes
+import { welcome} from './components/welcome.js';
+const divRoot= document.getElementById('root')
+const routes = {
+    '/': welcome,    
+};
+
+const onNavigate = (pathname) => {
+    window.history.pushState(
+        {},
+        pathname,
+        window.location.origin + pathname,
+    );
+    divRoot.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+divRoot.appendChild(component());
+
+
+
+
