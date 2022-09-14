0b4e7892532b212d7b684ddf26c54c800939e3bd
Paola Hernandez <hernandez.paolac@gmail.com>
2022-09-13 21:51:14 -0500

    Arreglando errores de ESlint en el main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-// Este es el punto de entrada de tu aplicacion
 import { iniciarSesion } from './components/login.js';
-//import { register } from './components/register.js';
-document.getElementById("root").innerHTML=iniciarSesion().outerHTML;
 
-//document.getElementById("root").innerHTML=register().outerHTML;
-
+document.getElementById('root').innerHTML = iniciarSesion().outerHTML;
