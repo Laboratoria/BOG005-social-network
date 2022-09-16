aee1a5bc05c30915856a04b2ff47defb580f9b9d
Sofia <81938344+Sofia0422@users.noreply.github.com>
2022-09-16 10:22:38 -0500

    incluye creacion de contenedores de nodos para adición de estilo CSS

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,36 +1,67 @@
 import { onNavigate } from '../main.js';
 
 export const Welcome = () => {
-  // contenedor que almacenará los 2 botones y dará un solo return
-  const div = document.createElement('header');
+  // contenedor que almacenará header, las 2 secciones y dará un solo return
+  const div = document.createElement('div');
   div.className = 'container';
-  // se crea titulo y parrafo
+
+  // Header
+  const header = document.createElement('header');
+  header.className = 'header';
   const title = document.createElement('h1');
   title.className = 'title';
+  title.textContent = 'Yummy Friends';
   const paragraph = document.createElement('h2');
   paragraph.className = 'paragraph';
-
-  // Mensaje Para iniciar Sesión
+  paragraph.textContent = 'Aquí encontrarás ese dulce que le hace falta a  tu vida.';
   const signIn = document.createElement('h2');
   signIn.className = 'signIn';
   signIn.textContent = 'Iniciar Sesión';
-  // Se crean 2 botones
-  const buttonLogin = document.createElement('button');
-  const buttonRegister = document.createElement('button');
+  header.append(title, paragraph, signIn);
 
+  // Se crea contenedor para inicio de sesión con correo, google y facebook
+  const section1 = document.createElement('section');
+  section1.className = 'section1';
+  const inputEmail = document.createElement('input');
+  inputEmail.setAttribute('requiered', '');
+  inputEmail.setAttribute('placeholder', 'Correo electrónico');
+  inputEmail.setAttribute('id', 'emailLogin');
+  const inputPass = document.createElement('input');
+  inputPass.setAttribute('requiered', '');
+  inputPass.type = 'password';
+  inputPass.setAttribute('placeholder', 'Contraseña');
+  inputPass.setAttribute('id', 'passwordLogin');
+  const buttonLogin = document.createElement('button');
   buttonLogin.textContent = 'Inicia Sesión';
-  buttonRegister.textContent = 'Regístrate';
-  title.textContent = 'Yummy Friends';
-  paragraph.textContent = 'Aquí encontrarás ese dulce que le hace falta a  tu vida.';
+  const google = document.createElement('a');
+  google.setAttribute('href', '');
+  google.textContent = 'Iniciar sesión con google';
+  google.className = 'google';
+  const facebook = document.createElement('a');
+  facebook.setAttribute('href', '');
+  facebook.textContent = 'Iniciar sesión con facebook';
+  facebook.className = 'facebook';
+  section1.append(inputEmail, inputPass, buttonLogin, google, facebook);
+
+  // Se crea contendeor pra registrarse si no tien una cuenta
+  const section2 = document.createElement('section');
+  section2.className = 'section2';
+  const account = document.createElement('p');
+  account.textContent = '¿No tienes cuenta?';
+  account.className = 'account';
+  const linkRegister = document.createElement('a');
+  linkRegister.setAttribute('href', '');
+  linkRegister.textContent = 'Regístrate';
+  section2.append(account, linkRegister);
 
   buttonLogin.addEventListener('click', () => {
     onNavigate('/login');
   });
-  buttonRegister.addEventListener('click', () => {
+  linkRegister.addEventListener('click', () => {
     onNavigate('/register');
   });
 
-  div.append(title, paragraph, signIn, buttonLogin, buttonRegister);
+  div.append(header, section1, section2);
 
   return div;
 };
