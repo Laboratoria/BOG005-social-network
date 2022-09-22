4c41da2949be0fbfb0922e03fa9a11d99c0e8444
Adriana Giraldo <1531nana@gmail.com>
2022-09-22 09:57:17 -0500

    inicializamos firestore,autenticación y agregamos a eslint los módulos

--- src/auth/authentication.js
+++ src/auth/authentication.js
@@ -1,1 +1,5 @@
-// autenticación
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
+import { app } from '../config/configFireBase.js';
+
+// const auth = getAuth();
+export const auth = getAuth(app);
