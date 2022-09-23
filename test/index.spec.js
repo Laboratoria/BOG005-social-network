7e36e0a065f169ab534530815945e4afe841bb06
Aleja22LC <alejaleona@hotmail.com>
2022-09-23 17:58:30 -0500

    Se sube el primer desarrollo de test para la funcion onNavigate, se mockea los archivos dentro de la carpeta firebase donde se exportan sus respectivas variables

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,32 @@
-// importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+// describe('myFunction', () => {
+//   it('debería ser una función', () => {
+//     expect(typeof myFunction).toBe('function');
+//   });
+// });
+import { onNavigate } from '../src/main.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+jest.mock('../src/firebase/connection.js');
+jest.mock('../src/firebase/auth.js');
+
+const mockLogin = () => {
+  const containerLogin = document.createElement('section');
+
+  const title = document.createElement('h1');
+  title.textContent = 'MOCK ENJOY THE WORLD';
+
+  containerLogin.append(title);
+  return containerLogin;
+};
+
+const mockRoutes = {
+  '/login': mockLogin,
+};
+
+describe('onNavigate', () => {
+  it('pintar el template', () => {
+    document.body.innerHTML = '<div id="root"></div>';
+    onNavigate('/login', mockRoutes);
+    console.log('documento root: ', document.getElementById('root').textContent);
+    expect(document.getElementById('root').textContent).toEqual('MOCK ENJOY THE WORLD');
   });
 });
