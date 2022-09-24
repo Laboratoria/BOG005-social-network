f31be37e2e8db1532e4575248791d40cfc198837
NattaBravo <109088297+NattaBravo@users.noreply.github.com>
2022-09-24 12:04:34 -0500

    Historia de usuario #1 Trazado de ruta para registro.

---
+++ src/pages/feed.js
@@ -0,0 +1,8 @@
+export const Feed = () => {
+    const view = `
+    <div class= "feedView">
+    <h1>Error 404</h1>
+    </div>
+    `;
+    return view;
+}
---
+++ src/pages/login.js
@@ -0,0 +1,16 @@
+const Login = `
+    <div id="backgroundVideo">    
+      <video src="./resources/pexels-vanessa-loring-5858145.mp4" autoplay loop muted></video>
+    </div>
+      <section id="sectionLogIn" class="sectionLogIn">
+        <input type="text" id="emailUser" placeholder="Correo electronico">
+        <input type="text" id="passWordUser" placeholder="Contraseña">
+        <a href="">¿Olvidaste tu contraseña?</a>
+        <button id="logginButton"  ;">ENTRAR</button>
+    </section>
+    <div id="registrarse">
+      <p>CUENTA</p>
+      <a href=""> Registrate!</a>
+    </div>
+  `
+export {Login};
