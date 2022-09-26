2f84e6a66aa9caa6f0837c6dc8ba79619bbc1817
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2022-09-26 12:31:07 -0500

    Corriginedo estilos en mobile, y modularizando

--- src/firebase/firestoreFirebase.js
+++ src/firebase/firestoreFirebase.js
@@ -1,18 +1,12 @@
-import { getFirestore, collection, addDoc, getDocs, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
-import { app } from './configFirabese.js';
-// getFirestore();
 
-
-// trae la conexión con firestone
-// db => trae un objeto con la conexión a firestore
-const db = getFirestore(app);
-console.log('firestone',db)
+import { collection, addDoc, getDocs, onSnapshot, db } from '../lib/firebaseIntermadiate/firestore.js'
 
 // colección crea la coleeción de datos
 const savePost = ( description ) => {
   addDoc(collection(db, 'Posts'), {description})
 }
 
+// obtenemos los post y recoremos la respuesta de firestore
 const getPost =  () => {
  getDocs(collection(db, "Posts")).then((res)=> {
   if( window.location.pathname === '/wall'){
@@ -41,28 +35,11 @@ const getPost =  () => {
       <i class="fa-solid fa-comment-dots" id="commet"></i>
     </div>
     </section>`;
-    // doc.data() is never undefined for query doc snapshots
-    // console.log(doc.id, " =>***xyz ", doc.data());
-    // console.log(doc.data().description);
-  })}
-}
- })
+    })}
+}})
 }
 
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   getDocs(collection(db, "Posts")).then((res)=> {
-//   const onGetPost = (res)=> { onSnapshot(collection(db, 'Posts'), callback)
-// console.log('ongetpost',onGetPost)
-// const contentedor = document.getElementById('allPosts')
-//  contentedor = ''
-// if(contentedor){
-//   contentedor.innerHTML +=`<p> ${doc.data().description}</p>`;
-// }
-//  }
-// })})
-
+// se muestran en pantalla al instante
 const onGetPost = ()=> {
    onSnapshot(collection(db, 'Posts'), (querySanpshot)=>{
     const contentedor = document.getElementById('postsContainerId')
@@ -94,18 +71,11 @@ const onGetPost = ()=> {
     // console.log('QUERY',querySanpshot)
   })
   //  console.log(querySanpshot)
-  
   };
 
 onGetPost();
 
-// db.collection("Posts").get().then((querySnapshot) => {
-//   querySnapshot.forEach((doc) => {
-//       console.log(`${doc.id} => ${doc.data()}`);
-//   });
-// });
-// getPost();
-export {getFirestore, savePost, getPost }
+export {savePost, getPost }
 
 //ICONO CORAZON RELLENO
 // <i class="fa-solid fa-heart" id="heart"></i>
