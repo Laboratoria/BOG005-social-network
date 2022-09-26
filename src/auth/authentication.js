5c787c5e00ae3396cd66d497355cf8fb7bdc9390
Adriana Giraldo <1531nana@gmail.com>
2022-09-26 12:36:13 -0500

    css a login, crear usuario y contraseña firebase, ruta login-register y template de login

--- src/auth/authentication.js
+++ src/auth/authentication.js
@@ -1,5 +1,29 @@
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
-import { app } from '../config/configFireBase.js';
-
-// const auth = getAuth();
-export const auth = getAuth(app);
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+  } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+  // import { Register } from "../components/registers.js";
+  import { app } from "../config/configFireBase.js";
+  import { onNavigate } from "../main.js";
+  import { Login } from "../pages/login.js";
+  const auth = getAuth(app);
+  const root = document.getElementById('root');
+  
+  const createEmail = (email, password) => {
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        root.innerHTML = Login
+        onNavigate('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        onNavigate("/register");
+      });
+  };
+  
+  export { createEmail };
+  
