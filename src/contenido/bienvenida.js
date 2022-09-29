0a689a45c67cfb232d169a3e8d3df740a2e945e3
JenifferBarrios <jbarrios03@hotmail.com>
2022-09-29 18:25:07 -0500

    Se logró el botón para ingresar con cuenta Google, botón cerrar sesión y además estilos a la vista Bienvenida

--- src/contenido/bienvenida.js
+++ src/contenido/bienvenida.js
@@ -1,19 +1,21 @@
 // eslint-disable-next-line import/no-cycle
 import { onNavigate } from '../main.js';
-
 export const bienvenida = () => {
   const div = document.createElement('div');
+  div.id ='contenedor-bienvenida';
   const titulo = document.createElement('p');
+  titulo.id = 'texto-bienvenida';
   const botonInicio = document.createElement('button');
   botonInicio.id = 'explora';
   const logo = document.createElement('img');
   logo.src = '/imagenes/Recurso 1.png';
+  logo.id = 'logo-bienvenida';
   titulo.innerHTML = '<b>Atrévete</b> a explorar el realismo mágico de nuestra <b> geografía Colombiana.</b> <br> <b>Guarda tus memorias</b> y haz que mas personas se animen a conocerlas';
   botonInicio.textContent = 'Explora';
   botonInicio.addEventListener('click', () => {
     onNavigate('/registro');
   });
 
-  div.append(logo, titulo, botonInicio);
+  div.append( logo,titulo, botonInicio);
   return div;
 };
