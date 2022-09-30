769a4b5eb19e049bbd0037e3d6e6cf2cbab486ac
SADYSCASTROC <sadyscastro@gmail.com>
2022-09-30 11:30:34 -0500

    stilos

--- src/components/login.js
+++ src/components/login.js
@@ -11,10 +11,11 @@ export const login = () => {
 			<figcaption>Leyenda de imágen del contenido de la pagina</figcaption>
 		</figure>
 		<form class="formDatos">
-			<input type="text" name="" id="emailLogin" class="camapoTexto" placeholder="Correo">
-			<input type="password" pattern=".{6,}" name="" id="paswordLogin" class="camapoTexto" placeholder="Contraseña">
-			<p id="passwordhidden">las credenciales no coinciden</p>
-			<button class="buttonIniciar" type="submit">Iniciar sesion</button>
+			<input type="text" name="" id="emailLogin" class="texField" placeholder="Email">
+			<p id="gmailNotFound">gmail not found</p>
+			<input type="password" pattern=".{6,}" name="" id="passwordLogin" class="texField" placeholder="Password">
+			<p  id="invalidPassword">Invalid password</p>
+			<button class="buttonStar" type="submit">Log in</button>
 		</form>
 		<button class="button googleLogin" "type="submit">Google</button>
 		<p class="registrarse">¿No tienes cuenta?<a href="#register">Registrate</a></p>
@@ -23,6 +24,7 @@ export const login = () => {
 	</div>;      
   
    </section>`;
+<<<<<<< Updated upstream
 
    const googleButt = sectionLogin.querySelector('.googleLogin');
    googleButt.addEventListener('click', () => {
@@ -39,3 +41,44 @@ export const login = () => {
 
             
    
+=======
+  const invalidPasswor = sectionLogin.querySelector('#invalidPassword');
+  const gmailNotFoun = sectionLogin.querySelector('#gmailNotFound');
+  const loginForm = sectionLogin.querySelector('.formDatos');
+  const loginEmail = sectionLogin.querySelector('#emailLogin');
+  const loginPassword = sectionLogin.querySelector('#passwordLogin');
+  
+  gmailNotFoun.style.display = 'none';
+  invalidPasswor.style.display = 'none';
+  loginForm.addEventListener('submit', () => {
+    loginUser(loginEmail.value, loginPassword.value)
+      .then(() => {
+		
+		  // console.log("token "+userCredential["user"]["accessToken"])
+		    window.location.hash = '#wall';
+		    loginForm.reset();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+			 if (errorCode === 'auth/user-not-found') {
+				gmailNotFoun.style.display = 'block';
+
+			 } else if (errorCode === 'auth/wrong-password') {
+				invalidPasswor.style.display = 'block';
+        }
+		  });
+  });
+
+  const googleButt = sectionLogin.querySelector('.googleLogin');
+  googleButt.addEventListener('click', () => {
+    googleSignIn()
+      .then((result) => {
+        const user = result.user;
+        window.location.hash = '#wall';
+        saveUserInfo(user.email, user.email, user.uid);
+      });
+  });
+
+  return sectionLogin;
+};
+>>>>>>> Stashed changes
--- src/components/register.js
+++ src/components/register.js
@@ -3,6 +3,7 @@
 
 
 export const register = () => {
+<<<<<<< Updated upstream
     const registrar = document.createElement("section");
     registrar.className="login"
     registrar.innerHTML =
@@ -24,10 +25,36 @@ export const register = () => {
        
       </div> 
        `
+=======
+  const registrar = document.createElement('section');
+  registrar.className = 'register';
+  registrar.innerHTML = `
+        <div class="container">
+        <figure class="imageDescription">
+        <img class="imgLogo" src="IMG/Explore-removebg-preview.png">
+        <h1>Create Account </h1>
+        </figure> 
+        <form action=""  id="registerForm">
+        <input type="text" name="" id="nameRegister" class="texField" placeholder="Name">
+          <input type="text" name="" id="emailRegister" class="texField" placeholder="Email">
+          <p id="mailInSuso">Gmail in use</p>
+          <input type="password" pattern=".{6,}" name="" id="passwordRegister" class="texField" placeholder="Password">
+          <button  class="button register__button-google"  "type="submit">
+          Sign up</button>
+          <p class="youAreMember">
+          Are you already a member?</p>
+          <p class="startSession"> <a href="#login" class="a">
+          Login</a> </p>
+        </form>
+       
+      </div> 
+       `;
+  const emailUSo = registrar.querySelector('#mailInSuso');
+>>>>>>> Stashed changes
   const registerForm = registrar.querySelector('#registerForm');
-  const registerUsername = registrar.querySelector('#nameRegister');
   const registerEmail = registrar.querySelector('#emailRegister');
   const registerPassword = registrar.querySelector('#passwordRegister');
+<<<<<<< Updated upstream
 
   registerForm.addEventListener('submit', (event) => {
     
@@ -47,6 +74,27 @@ export const register = () => {
        console.log(errorCode);
      
     });
+=======
+  emailUSo.style.display = 'none';
+  registerForm.addEventListener('submit', () => {
+    createUser(registerEmail.value, registerPassword.value)
+      .then((userCredential) => {
+      // console.log("token "+userCredential["user"]["accessToken"])
+        const user = userCredential.user;
+        saveUserInfo(user.email, user.email, user.uid);
+        // alert('usuario registrado correctamente');
+
+        window.location.hash = '#wall';
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        if(errorCode === "auth/email-already-in-use"){
+
+          emailUSo.style.display = 'block';
+          
+        }
+      });
+>>>>>>> Stashed changes
   });
           return registrar; 
        };
