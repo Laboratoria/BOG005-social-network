7c0474871b24a9907793bd93ecdfd4cae39a8e64
SoreyC <soreycortes@gmail.com>
2022-10-01 16:00:58 -0500

    Test unitarios de ruta 100% solo en wall

--- src/firebase/__mocks__/firetoreFirebase.js
+++
@@ -1,7 +0,0 @@
-// export const savePost = () => Promise.resolve();
-// export const getPost =  () => Promise.resolve();
-// export const onGetPost = ()=> Promise.resolve();
-// export const onSnapshot = () => Promise.resolve();
-// export function collection() {
-//     return Promise.resolve();
-// }
