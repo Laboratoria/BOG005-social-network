dc62ef6f5ebc7650722299a44e40f2c2e5f22895
Ale Salazar <alxsalaza@gmail.com>
2022-10-03 21:47:12 -0500

    Este codigo registra al usuario en FBase y despliega alertas por cada error. Cuando es correcto el registro, lleva al muro, el problema es que cuando es equivocado también

--- src/components/register.js
+++ src/components/register.js
@@ -1,51 +1,33 @@
 import { onNavigate } from '../main.js';
-import { createRegister } from '../firebase/auth.js';
+import { createUser } from '../firebase/connection.js';
 
 export const register = () => {
   const containerRegister = document.createElement('section');
   containerRegister.classList.add('container');
 
   const title = document.createElement('h1');
-  title.textContent = 'REGISTRATE';
-  title.className = 'titleR';
+  title.textContent = 'REGÍSTRATE';
+  title.className = 'title';
 
   const registerForm = document.createElement('section');
   registerForm.classList.add('formRegister');
 
-  const inputName = document.createElement('input');
-  inputName.classList.add('input');
-  inputName.setAttribute('type', 'text');
-  inputName.setAttribute('id', 'inputName');
-  inputName.setAttribute('placeholder', 'Nombre');
-  inputName.setAttribute('required', '');
-
-  const inputLastName = document.createElement('input');
-  inputLastName.classList.add('input');
-  inputLastName.setAttribute('type', 'text');
-  inputLastName.setAttribute('id', 'inputLastName');
-  inputLastName.setAttribute('placeholder', 'Apellido');
-  inputLastName.setAttribute('required', '');
-
   const registerEmail = document.createElement('input');
   registerEmail.classList.add('input');
   registerEmail.setAttribute('type', 'email');
-  registerEmail.setAttribute('id', 'emailRegister');
   registerEmail.setAttribute('placeholder', 'E-mail');
   registerEmail.setAttribute('required', '');
-  
 
   const registerPassword = document.createElement('input');
   registerPassword.classList.add('input');
   registerPassword.setAttribute('type', 'password');
-  registerPassword.setAttribute('id', 'passwordRegister');
   registerPassword.setAttribute('placeholder', 'contraseña');
   registerPassword.setAttribute('required', '');
-  
+
   const registerButton = document.createElement('button');
-  // otra forma de agregar atributos a un nodo
+  registerButton.textContent = 'Inicia tu Viaje';
+  registerButton.setAttribute('class', 'button');
   registerButton.id = 'registerButton';
-  registerButton.textContent = 'Iniciar Sesión';
-  registerButton.setAttribute('class', 'buttonRegister button');
 
   const question = document.createElement('h3');
   question.textContent = '¿Olvidaste tu Contraseña?';
@@ -56,57 +38,38 @@ export const register = () => {
   session.className = 'here';
 
   registerButton.addEventListener('click', () => {
-    const emailR = registerEmail.value;
-    const passR = registerPassword.value;
-    console.log(createRegister(emailR, passR));
+    const emailRegister = registerEmail.value;
+    const passRegister = registerPassword.value;
+    console.log(emailRegister, passRegister);
 
-    createRegister(emailR, passR)
+    const createRegister = async(email, password) => {;
+      try {
+        createUser(email, password);
+      }
+      catch(error){
+        return error;
+      }
+    };
+  
+    createRegister(emailRegister, passRegister)
       .then(() => {
-        // console.log('dentroooo');
+        console.log('dentroooo');
         onNavigate('/wall'); // si ya se registró que entre a muro
       })
-      .catch(() => {});
-      onNavigate('/register');
-  });
-
-  const errorText = document.createElement('p');
-  errorText.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const emailRegister = registerEmail.value;
-    const passwordRegister = registerPassword.value;
-    
-    createRegister(emailRegister, passwordRegister)
-      .then(() => {
-        onNavigate('/wall');
-      })
-      .catch((error) => {
-        onNavigate('/register');
-        const errorCode = error.code;
-        if (errorCode === 'auth/email-already-in-use') {
-          onNavigate('/register');
-          errorText.textContent = 'El e-mail ingresado ya existe';
-          onNavigate('/register');
-        } else if (errorCode === 'auth/weak-password') {
-          onNavigate('/register');
-          errorText.textContent = 'Su contraseña debe tener al menos 6 caracteres';
-          onNavigate('/register');
-        } else if (errorCode === 'auth/invalid-email') {
-          errorText.textContent = 'No es un e-mail válido';
-        }
+      .catch(() => {
+        console.log('fail');
+        onNavigate('/register')
       });
-  });
+    });
 
   containerRegister.append(registerForm);
   registerForm.append(
     title,
-    inputName,
-    inputLastName,
     registerEmail,
     registerPassword,
     registerButton,
     question,
     session,
-    errorText,
   );
 
   return containerRegister;
--- src/firebase/connection.js
+++ src/firebase/connection.js
@@ -1,103 +1,112 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import { getAuth, 
-         createUserWithEmailAndPassword, 
-         signInWithEmailAndPassword, 
-         onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5L_8-iWK_fcuTnIWV4peHFJMmOL8v7Qo",
-  authDomain: "enjoy-the-world.firebaseapp.com",
+  apiKey: 'AIzaSyB5L_8-iWK_fcuTnIWV4peHFJMmOL8v7Qo',
+  authDomain: 'enjoy-the-world.firebaseapp.com',
   databaseURL: "https://enjoy-the-world-default-rtdb.firebaseio.com",
-  projectId: "enjoy-the-world",
-  storageBucket: "enjoy-the-world.appspot.com",
-  messagingSenderId: "976150175703",
-  appId: "1:976150175703:web:009703b036b923ca747592",
-  measurementId: "G-R81NHVQFFX"
+  projectId: 'enjoy-the-world',
+  storageBucket: 'enjoy-the-world.appspot.com',
+  messagingSenderId: '976150175703',
+  appId: '1:976150175703:web:9812817e989a64db747592',
+  measurementId: 'G-DWY854X4RL',
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); // **En otras versiones se eliminó la variable
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app); // **En otras versiones se elimina el parámetro.
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const createUser = (email, password) => {
-  
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      swal({
-        title: "Genial!",
-        text: "Conseguiste registrarte!",
-        icon: "success",
-        button: "Inicia tu viaje!",
-      });
-      // TODO: verificar por qué entra primero al catch porque imprime por unos segundos el mensaje de fail
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if(errorCode === "auth/email-already-in-use"){
-        swal({
-          title: "Por favor verifica,",
-          text: "El usuario ya existe",
-          icon: "error",
-        });
-      }
-      else if(errorCode === "auth/invalid-email"){
-        swal({
-          title: "Ingresa un email válido, por favor.",
-          text: "debe tener formato de email",
-          icon: "error",
-        });
-      }
-      else if(errorCode === "auth/weak-password"){
-        swal({
-          title: "Tu contraseña es muy débil!,",
-          text: "Por favor usa más de seis caracteres",
-          icon: "error",
-        });
-      }       
+  .then((userCredential) => {
+    // signed in
+    const user = userCredential.user;
+    console.log(user);
+    console.log('oh yeah');
+    swal({
+      title: "Genial!",
+      text: "Conseguiste registrarte!",
+      icon: "success",
+      button: "Inicia tu viaje!",
     });
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    // const errorMessage = error.message;
+    console.log('ay no!')
+    if(errorCode === "auth/email-already-in-use"){
+      swal({
+        title: "Por favor verifica,",
+        text: "El usuario ya existe",
+        icon: "error",
+      });
+    }
+    else if(errorCode === "auth/invalid-email"){
+      swal({
+        title: "Ingresa un email válido, por favor.",
+        text: "debe tener formato de email",
+        icon: "error",
+      });
+    }
+    else if(errorCode === "auth/weak-password"){
+      swal({
+        title: "Tu contraseña es muy débil!,",
+        text: "Por favor usa más de seis caracteres",
+        icon: "error",
+      });
+    }       
+  });
 };
 
 const signInUser = (email, password) => {
   console.log('email: ', email, 'password: ', password);
-   return signInWithEmailAndPassword(auth, email, password)
+  return signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-   console.log(user, 'Signed in');
+    console.log(user, 'Signed in');
+    // signed in
     const user = userCredential.user;
-    swal("Llegaste a tu muro!", "Aquí comienza tu viaje", "success");
-    // ...
+    //  ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     console.log(errorMessage);
-    swal("Upps!", "Algo salió mal!", "error");
   });
 };
 
-  onAuthStateChanged(auth, (user) => {
-    if (user != null) {
-      console.log('Conected');
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      // ...
-    } else {
-      console.log('No conection');
-      // User is signed out
-      // ...
-    }
-  });
+// Detectando el estado de autenticación
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log('In Firebase');
+    const uid = user.uid;
+  } else {
+    console.log('Not in Firebase');
+  }
+});
 
-export {auth, createUser, signInUser};
-// ***1.Importar el código inicial provisto por Firebase (línea 1 y de 4 hasta 15 ) esa es la conexión con FBase y la id única de la App.
-// ***2.Importar el código de autenticación, el de crear usuario y el de loguear usuario línea y el del observador 2 (getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged) 
-// ese se puede encontrar en la documentación como: firebase/documentos/comenzar o primeros pasos.
-// ***3.Copiar el código con los métodos createUserWithEmailAndPassword y signInWithEmailAndPassword que se encuentran en esa misma página. (ambas son promesas)
-// las reconocen porque tienen un .then y un .catch. Línea de 20 hasta la 41 
-// ***4.Pegar el código de onAuthStateChanged que aparece en la misma página, es una condicional línea 44 a 54.
-// ***5.Crear variable con función flecha que crea al usuario y que engloba la promesa. se hace con el createUserWithEmailAndPassword y el signInWithEmailAndPassword.
-// ***6.Crearíamos un archivo .js para la autenticación... ir a auth.js.
+// signOut(auth).then(() => {
+//   // signout successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
+
+// const user = auth.currentUser;
+// if (user !== null) {
+//   // The user object has basic properties such as display name, email, etc.
+//   const displayName = user.displayName;
+//   const email = user.email;
+//   const photoURL = user.photoURL;
+//   const emailVerified = user.emailVerified;
+
+  // The user's ID, unique to the Firebase project. Do NOT use
+  // this value to authenticate with your backend server, if
+  // you have one. Use User.getToken() instead.
+//   const uid = user.uid;
+// }
+
+export { auth, createUser, signInUser };
