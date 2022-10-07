173794fa0f84fbbd570225ee69b43ecb6f047b3c
Aleja22LC <alejaleona@hotmail.com>
2022-10-07 12:26:40 -0500

    se sube el boton de exit para darle su funcionalidad

--- src/components/wall.js
+++ src/components/wall.js
@@ -4,24 +4,29 @@ export const wall = () => {
   const containerWall = document.createElement('section');
   containerWall.classList.add('container');
 
-  const header = document.createElement ('header')
+  const header = document.createElement('header')
   header.classList.add('header');
-  
-  const iconExit = document.createElement('img');
-  iconExit.src = 'https://img.icons8.com/external-prettycons-flat-prettycons/47/000000/external-exit-essentials-prettycons-flat-prettycons.png';
-  iconExit.id = 'iconExit';
+
+  const buttonExit = document.createElement('button');
+  buttonExit.classList.add('buttonIcons');
+  buttonExit.id = 'btnExit';
+
+  // const iconExit = document.createElement('img');
+  // iconExit.src = 'img/exit.png';
+  // iconExit.alt = 'Exit';
+  // iconExit.id = 'iconExit';
 
   const imgTitle = document.createElement('img');
   imgTitle.src = 'img/other_enjoy-removebg-preview.png';
   imgTitle.alt = 'Logo';
   imgTitle.id = 'imgTitle';
 
-  const title = document.createElement('h1');
-  title.textContent = '¿Qué festividad se celebra hoy en tu ciudad?';
-  title.id = 'titleWall';  
+  // const title = document.createElement('h1');
+  // title.textContent = '¿Qué festividad se celebra hoy en tu ciudad?';
+  // title.id = 'titleWall';
 
-  const wallForm = document.createElement('article');
-  wallForm.classList.add('wallForm');
+  const wallFormContainer = document.createElement('article');
+  wallFormContainer.classList.add('wallForm');
 
   const wallPost = document.createElement('textarea');
   wallPost.classList.add('post');
@@ -30,13 +35,20 @@ export const wall = () => {
   wallPost.setAttribute('rows', '4');
   wallPost.setAttribute('placeholder', ' Tu post aquí');
 
-  const icon = document.createElement('img');
-  icon.src = 'https://img.icons8.com/external-inkubators-glyph-inkubators/25/FCAB2E/external-send-ecommerce-user-interface-inkubators-glyph-inkubators.png';
-  icon.id = 'iconSend';
+  const iconContainer = document.createElement('article');
+  iconContainer.setAttribute('id', 'postIcon');
 
-  header.append(imgTitle, title, iconExit)
-  wallForm.append(wallPost)
-  containerWall.append(header, wallForm, icon);
+  const iconForm = document.createElement('article');
+  iconForm.classList.add('iconForm');
+
+  const iconSend = document.createElement('img');
+  iconSend.src = 'img/send.png';
+  iconSend.id = 'iconSend';
+
+  header.append(imgTitle, buttonExit)
+  wallFormContainer.append(wallPost, iconContainer)
+  iconContainer.append(iconSend)
+  containerWall.append(header, wallFormContainer);
 
   return containerWall;
 };
