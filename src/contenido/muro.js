17eb3f20de9296685a3d17549c973d0997cc472d
ynneyavlis <yennysilvariveros@gmail.com>
2022-10-11 15:02:39 -0500

    Datos de fibase

--- src/contenido/muro.js
+++ src/contenido/muro.js
@@ -1,71 +1,77 @@
 import { cerrarSesion } from "../lib/firebase.js";
-import { onSnapshot, pubRef } from "../lib/firestore.js";
-import { guardarPublicaciones } from "../lib/firestore.js"
+import { pubRef } from "../lib/firestore.js";
+import { guardarPublicaciones } from "../lib/firestore.js";
 
+
+// onSnapshot(pubRef(), (querySnapshot) => {
+// console.log(querySnapshot)
+// querySnapshot.forEach((element) => {
+// console.log(element);
+// });
+// });
+
+
+//Creacion de elementos o nodos en el muro
 export const muro = () => {
-  onSnapshot(pubRef(), (querySnapshot) => {
-    // console.log(querySnapshot)
-    querySnapshot.forEach((element) => {
-      console.log(element);
-    });
-
-  });
-  //Creacion de elementos o nodos en el muro
-  const div = document.createElement('div');
-  div.id = 'contenedor-muro';
-  const logo = document.createElement('img');
-  logo.id = 'muro-logo';
-  logo.src = '/imagenes/Recurso 1.png';
-  const tituloP = document.createElement("p");
-  tituloP.id = "texto-publicar";
-  const crearPublicacion = document.createElement("textarea");
-  crearPublicacion.id = "contenido-publicar";
-  const buttonPublicar = document.createElement("button");
-  buttonPublicar.id = "creacionPublicar";
-  const buttonX = document.createElement("button");
-  buttonX.id = "cerrar-publicar";
-  const salir = document.createElement("button");
-  salir.id = "salir";
-  const casita = document.createElement("button");
-  casita.class = "casita";
-  const perfil = document.createElement("button");
-  perfil.class = "perfil";
+const div = document.createElement('div');
+div.id = 'contenedor-muro';
+const logo = document.createElement('img');
+logo.id = 'muro-logo';
+logo.src = '/imagenes/Recurso 1.png';
+const tituloP = document.createElement("p");
+tituloP.id = "texto-publicar";
+const crearPublicacion = document.createElement("textarea");
+crearPublicacion.id = "contenido-publicar";
+const buttonPublicar = document.createElement("button");
+buttonPublicar.id = "creacionPublicar";
+const buttonX = document.createElement("button");
+buttonX.id = "cerrar-publicar";
+const salir = document.createElement("button");
+salir.id = "salir";
+const casita = document.createElement("button");
+casita.class = "casita";
+const perfil = document.createElement("button");
+perfil.class = "perfil";
 
 
-  salir.textContent = "Cerrar Sesión";
-  // casita.innerHTML = "<span class="material-icons"><p>Inicio</p></span>" ;
-  perfil.textContent = "Perfil";
-  tituloP.textContent = "Crear publicación";
-  crearPublicacion.setAttribute('placeholder', "¿Que has conocido hoy de Colombia?");
-  buttonPublicar.textContent = "Publicar";
+salir.textContent = "Cerrar Sesión";
+// casita.innerHTML = "<span class="material-icons"><p>Inicio</p></span>" ;
+perfil.textContent = "Perfil";
+tituloP.textContent = "Crear publicación";
+crearPublicacion.setAttribute('placeholder', "¿Que has conocido hoy de Colombia?");
+buttonPublicar.textContent = "Publicar";
 
-  // Crear la funcion salir en firebase.js
-  // Devuelva la metodo de firebase signOut()
-  //Importar en muro 
-  // añadir un manejador de eventos al boton salir
-  // Dentro de la funcion del addEventListener llamar a la funcion salir
+// Crear la funcion salir en firebase.js
+// Devuelva la metodo de firebase signOut()
+//Importar en muro 
+// añadir un manejador de eventos al boton salir
+// Dentro de la funcion del addEventListener llamar a la funcion salir
 
-  const likes = [];
+const likes = [];
 
-  buttonPublicar.addEventListener('click', () => {
-    console.log('valor del input: ', crearPublicacion.value);
-    guardarPublicaciones(crearPublicacion.value, likes, "xyz123")
-      .then(() => onSnapshot(pubRef(), (querySnapshot) => {
-        querySnapshot.forEach((element) => {
-          console.log(element)
-          .catch((error) => console.error(error.message))
-        })
-      }))
-    })
-    
- salir.addEventListener('click', () => {
-      cerrarSesion().then(() => {
-      })
-      .catch((error) => {
-        console.error(error.message)
-      })
-    })
+buttonPublicar.addEventListener('click', async () => {
+  console.log('valor del input: ', crearPublicacion.value);
+  guardarPublicaciones(crearPublicacion.value, likes, "xyz123")
   
-  div.append(logo, salir, casita, perfil, tituloP, buttonX, crearPublicacion, buttonPublicar);
-  return div;
-}
+   const querySnapshot = await pubRef()
+  // querySnapshot.forEach( (doc) => {
+  console.log(querySnapshot)
+})
+//   .then(() => onSnapshot(pubRef(), (querySnapshot) => {
+//     
+//       .catch((error) => console.error(error.message))
+//     })
+//   }))
+// })
+
+salir.addEventListener('click', () => {
+  cerrarSesion().then(() => {
+  })
+    .catch((error) => {
+      console.error(error.message)
+    })
+})
+
+div.append(logo, salir, casita, perfil, tituloP, buttonX, crearPublicacion, buttonPublicar);
+return div
+};
--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -10,30 +10,7 @@ export const guardarPublicaciones = (post, likes, currentUser) => {
     currentUser
 
   })
-
-//   const pubRef = () => {
-//     const todospos = query(collection(db, "publicaciones"), orderBy("date", "desc"));
-//     then((querySnapshot) => {
-//       querySnapshot.forEach((doc) => {
-//         doc.data();
-//         console.log(doc.data());
-//         return getDocs(todospos);
-//       });   
-//   });
-// }
-};
-export const pubRef = () => query(collection(db, "publicaciones"), orderBy("date", "desc"));
-export {onSnapshot}
-// const q = query(collection(db, "publicaciones")
-// const unpost = onSnapshot(q, (querySnapshot) => {
-//   const arrPublicaciones = [];
-//   querySnapshot.forEach((doc) => {
-//     arrPublicaciones.push(doc.data().post);
-//   });
-//   console.log("mi pots ", arrPublicaciones.join(", "));
-// });
-
-// unpost();
-
+   }
+export const pubRef = () => getDocs (collection(db,'publicaciones'))
 
 
