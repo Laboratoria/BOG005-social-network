9e2003dc90ac2dfdf8ff722535434d448e8f83a8
Astrid-Macana <lorenamakna21@gmail.com>
2022-10-19 17:12:31 -0500

    alerta de post en blanco sin multiplicacion de post

--- src/contenido/muro.js
+++ src/contenido/muro.js
@@ -4,18 +4,16 @@ import { obtpost, borrarPost, editarPost, actualizarPost, addLike, removeLike, g
 
 //Creacion de elementos o nodos en el muro
 export const muro = () => {
+  const div = document.createElement("div");
+  div.id = "contenedor-muro";
+  const header = document.createElement("header");
+  header.id = "header-muro";
+  const comentario = document.createElement("div");
+  comentario.id = "comentario";
 
-  const div = document.createElement('div');
-  div.id = 'contenedor-muro';
-  const header = document.createElement('header');
-  header.id = 'header-muro';
-  const comentario = document.createElement('div');
-  comentario.id = 'comentario'
-  const footer = document.createElement('footer')
-  footer.id = "footer"
-  const logo = document.createElement('img');
-  logo.id = 'muro-logo';
-  logo.src = '/imagenes/Recurso 1.png';
+  const logo = document.createElement("img");
+  logo.id = "muro-logo";
+  logo.src = "/imagenes/Recurso 1.png";
   const tituloP = document.createElement("p");
   tituloP.id = "texto-publicar";
   const crearPublicacion = document.createElement("textarea");
@@ -24,45 +22,45 @@ export const muro = () => {
   buttonPublicar.id = "creacionPublicar";
   const pubRecientes = document.createElement("p");
   pubRecientes.id = "pubRecientes";
-  const buttonX = document.createElement("button");
-  buttonX.id = "cerrar-publicar";
   const salir = document.createElement("button");
   salir.id = "salir";
-  const casita = document.createElement("button");
-  casita.class = "casita";
-  const perfil = document.createElement("button");
-  perfil.class = "perfil";
+  
 
-
-  salir.textContent = "Cerrar Sesión";
-  // casita.innerHTML = "<span class="material-icons"><p>Inicio</p></span>" ;
-  perfil.textContent = "Perfil";
-  tituloP.textContent = "Crear publicación";
-  crearPublicacion.setAttribute('placeholder', "¿Que has conocido hoy de Colombia?");
+ salir.textContent = "Cerrar Sesión";
+ tituloP.textContent = "Crear publicación";
+  crearPublicacion.setAttribute( "placeholder", "¿Que has conocido hoy de Colombia?" );
   buttonPublicar.textContent = "Publicar";
   pubRecientes.textContent = "Publicaciones Recientes";
 
   // Crear la funcion salir en firebase.js
   // Devuelva la metodo de firebase signOut()
-  //Importar en muro 
+  //Importar en muro
   // añadir un manejador de eventos al boton salir
   // Dentro de la funcion del addEventListener llamar a la funcion salir
-  let editEstatus = false
+  let editEstatus = false;
   let docId = "";
   const likes = [];
   // const contLikes = 0;
+  buttonPublicar.addEventListener("click", async () => {
+    if(crearPublicacion.value == "" ) {
+        return swal({
+            title: "Escribe tu publicacion!",
+            text: "El campo de publicar esta vacío",
+            icon: "error", 
+    })
+    }else{
+        await guardarPublicaciones(crearPublicacion.value, likes);
+    }
+   
+ }); //cierra boton publicar
 
-
-  buttonPublicar.addEventListener('click', async () => {
-    guardarPublicaciones(crearPublicacion.value, likes)
-    obtpost(querySnapshot => {
-      let elementosPost = ''
-      querySnapshot.forEach(doc => {
-
-        const info = doc.data()
-        //Contiene el caja para editar, borrar y guardar el post
-        let arrayString = info.likes.toString()
-        elementosPost += `
+  obtpost((querySnapshot) => {
+    let elementosPost = "";
+    querySnapshot.forEach((doc) => {
+      const info = doc.data();
+      //Contiene el caja para editar, borrar y guardar el post
+      let arrayString = info.likes.toString();
+      elementosPost += `
         <div> 
           <input class="input" id='id-${doc.id}' value="${info.post}" disabled/>
           <button class="btn-Borrar" data-id="${doc.id}">Borrar</button>
@@ -71,85 +69,88 @@ export const muro = () => {
           <button class="btn-Like" data-id="${doc.id}" data-like=${arrayString}>Like</button>
           <span>${info.likes.length}</span>
           </div>
-       `
-      })
+       `;
+    });
     // });
-      //limpiar textArea
-      crearPublicacion.value = '';
-      //Funcion Borrar Post 
-      comentario.innerHTML = elementosPost
-      const btnsBorrar = comentario.querySelectorAll('.btn-Borrar');
-      btnsBorrar.forEach(btn => {
-        btn.addEventListener("click", ({ target: { dataset } }) => {
-          borrarPost(dataset.id)
-        })
-      })  //ok
-      //likes
-      const btnLike = document.querySelectorAll(".btn-Like");
-      btnLike.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          console.log('id del usuario: ', getCurrentUser().uid);
-          const userId = getCurrentUser().uid;
-          //id de usuarios agregado al array likes, pero convertidos en string
-          const userIdsLikes =  e.target.dataset.like
-          if (!userIdsLikes.includes(userId)) { 
-            addLike(e.target.dataset.id, userId);
-          } else{ 
-            removeLike(e.target.dataset.id, userId);
-          }          
 
-          
-        });
-      });  //ok
-    
+    //limpiar textArea
+  crearPublicacion.value = ""; 
+    //Funcion Borrar Post
+    comentario.innerHTML = elementosPost;
+    const btnsBorrar = comentario.querySelectorAll(".btn-Borrar");
+    btnsBorrar.forEach((btn) => {
+      btn.addEventListener("click", ({ target: { dataset } }) => {
+        borrarPost(dataset.id);
+      });
+    }); //ok
+    //likes
+    const btnLike = document.querySelectorAll(".btn-Like");
+    btnLike.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        console.log("id del usuario: ", getCurrentUser().uid);
+        const userId = getCurrentUser().uid;
+        //id de usuarios agregado al array likes, pero convertidos en string
+        const userIdsLikes = e.target.dataset.like;
+        if (!userIdsLikes.includes(userId)) {
+          addLike(e.target.dataset.id, userId);
+        } else {
+          removeLike(e.target.dataset.id, userId);
+        }
+      });
+    }); //ok
+
     //Funcion Editar Post
-    const btnsEditar = comentario.querySelectorAll('.btn-Editar');
-    btnsEditar.forEach(btn => {
+    const btnsEditar = comentario.querySelectorAll(".btn-Editar");
+    btnsEditar.forEach((btn) => {
       btn.addEventListener("click", async (event) => {
-        docId = event.target.dataset.id
-        const doc = await editarPost(docId)
+        docId = event.target.dataset.id;
+        const doc = await editarPost(docId);
 
-        const inputEditar = comentario.querySelector(`#id-${docId}`)
-        console.log(inputEditar)
-        inputEditar.disabled = false
+        const inputEditar = comentario.querySelector(`#id-${docId}`);
+        console.log(inputEditar);
+        inputEditar.disabled = false;
         editEstatus = true;
-      })
-    })  //ok
+      });
+    }); //ok
     //Funcion Guardar Post
 
-    const btnsGuardar = comentario.querySelectorAll('.btn-Guardar');
-    btnsGuardar.forEach(btn => {
-
+    const btnsGuardar = comentario.querySelectorAll(".btn-Guardar");
+    btnsGuardar.forEach((btn) => {
       btn.addEventListener("click", (event) => {
-        const inputEditar = comentario.querySelector(`#id-${event.target.dataset.id}`)
-        console.log(inputEditar)
+        const inputEditar = comentario.querySelector(
+          `#id-${event.target.dataset.id}`
+        );
+        console.log(inputEditar);
         if (editEstatus) {
           editEstatus = false;
           actualizarPost(docId, {
-            post: inputEditar.value
-          })
+            post: inputEditar.value,
+          });
         }
+      });
+    }); //ok
+  });
 
-      })
-    }) //ok
-  })  //cierra boton publicar
-})
- 
-  salir.addEventListener('click', () => {
-    cerrarSesion().then(() => {
-    })
+  salir.addEventListener("click", () => {
+    cerrarSesion()
+      .then(() => {})
       .catch((error) => {
-        console.error(error.message)
-      })
-  })  //ok
-  window.addEventListener('DOMContentLoaded', () => {
-  })   //ok
+        console.error(error.message);
+      });
+  }); //ok
+  window.addEventListener("DOMContentLoaded", () => {}); //ok
 
   //Agregando elementos al div padre
   header.append(logo, salir);
   comentario.append(pubRecientes);
-  div.append(header, tituloP, crearPublicacion, buttonPublicar, comentario, footer);
-  footer.append(casita, perfil);
-  return div
-}
+  div.append(
+    header,
+    tituloP,
+    crearPublicacion,
+    buttonPublicar,
+    comentario,
+  );
+ 
+  return div;
+};
 
