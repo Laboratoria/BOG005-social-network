d280c2b07a4f03ef0cfe7625ef53c9924377ee9f
Astrid-Macana <lorenamakna21@gmail.com>
2022-10-24 17:44:43 -0500

    alerta de login lista

--- src/contenido/login.js
+++ src/contenido/login.js
@@ -1,57 +1,80 @@
-import { onNavigate } from '../main.js'; 
+import { onNavigate } from "../main.js";
 import { iniciarlogin, crearUsuarioConGoogle } from "../lib/firebase.js";
 
-
 export const login = () => {
-  const contenedorLogin = document.createElement('div');
-  contenedorLogin.id = 'contenedor-login'
+  const contenedorLogin = document.createElement("div");
+  contenedorLogin.id = "contenedor-login";
 
-  const logoLogin = document.createElement('img');
-  logoLogin.id = 'logologin';
-  const formularioLogin = document.createElement('form');
-  formularioLogin.id = 'formulario-login';
-  const correoLogin = document.createElement('input');
-  correoLogin.id = 'correo-login';
-  const contraseñaLogin = document.createElement('input');
-  contraseñaLogin.id = 'contraseña-login';
-  const buttoniniciaSesion = document.createElement('button');
-  buttoniniciaSesion.id = 'iniciarsesion-login';
-  const botonRegistro = document.createElement('button');
-  botonRegistro.id = 'registro-login';
-  const butonGoogle = document.createElement('button');
-  butonGoogle.id = 'Google';
+  const logoLogin = document.createElement("img");
+  logoLogin.id = "logologin";
+  const formularioLogin = document.createElement("form");
+  formularioLogin.id = "formulario-login";
+  const correoLogin = document.createElement("input");
+  correoLogin.id = "correo-login";
+  const contraseñaLogin = document.createElement("input");
+  contraseñaLogin.id = "contraseña-login";
+  const buttoniniciaSesion = document.createElement("button");
+  buttoniniciaSesion.id = "iniciarsesion-login";
+  const botonRegistro = document.createElement("button");
+  botonRegistro.id = "registro-login";
+  const butonGoogle = document.createElement("button");
+  butonGoogle.id = "Google";
 
-    buttoniniciaSesion.addEventListener('click', () => {
-      iniciarlogin (correoLogin.value,contraseñaLogin.value).then( () => {
-        console.log("iniciarlogin")
-        }).catch(()=>{
-          console.log("hay un error")
-        })
+  buttoniniciaSesion.addEventListener("click", () => {
+    iniciarlogin(correoLogin.value, contraseñaLogin.value)
+      .then(() => {
+        // console.log("iniciarlogin")
+        if (contraseñaLogin.value =='') {
+          swal({
+            title: "¡Escribe tu contraseña!",
+            text: "Tu contraseña debe tener mínimo seis dígitos",
+            icon: "error",
+          });
+        }
+        if (correoLogin.value == '') {
+          swal({
+            title: "¡Escribe tu correo!",
+            text: "Debes escribir un correo para ingresar",
+            icon: "error",
+          });
+        }
+      })
+      .catch(() => {
+        console.log("hay un error");
+      });
+  });
 
-         });
-       botonRegistro.addEventListener('click', () => {
-        onNavigate('/registro');
-    });
-    logoLogin.src = '/imagenes/Recurso22.png';
-    correoLogin.setAttribute('placeholder', 'Correo Electronico');
-    contraseñaLogin.setAttribute('placeholder', 'Contraseña Usuario');
-    contraseñaLogin.setAttribute("type","password");
-    buttoniniciaSesion.textContent = 'Iniciar Sesión';
-    botonRegistro.textContent = 'Registrate';
-    butonGoogle.textContent ='';  
-
+  botonRegistro.addEventListener("click", () => {
+    onNavigate("/registro");
+  });
+  logoLogin.src = "/imagenes/Recurso22.png";
+  correoLogin.setAttribute("placeholder", "Correo Electronico");
+  contraseñaLogin.setAttribute("placeholder", "Contraseña Usuario");
+  contraseñaLogin.setAttribute("type", "password");
+  buttoniniciaSesion.textContent = "Iniciar Sesión";
+  botonRegistro.textContent = "Registrate";
+  butonGoogle.textContent = "";
 
   //  para logiarce  con el boton de google
 
-    butonGoogle.addEventListener ("click",()=> {
-      crearUsuarioConGoogle ().then( () => {
-        console.log("crearUsuarioConGoogle")
-      }).catch(()=>{
-        console.log("hay un error")
+  butonGoogle.addEventListener("click", () => {
+    crearUsuarioConGoogle()
+      .then(() => {
+        console.log("crearUsuarioConGoogle");
       })
-      
+      .catch(() => {
+        console.log("hay un error");
       });
+  });
 
-      contenedorLogin.append(formularioLogin, logoLogin, correoLogin, contraseñaLogin, buttoniniciaSesion, botonRegistro, butonGoogle);
+  contenedorLogin.append(
+    formularioLogin,
+    logoLogin,
+    correoLogin,
+    contraseñaLogin,
+    buttoniniciaSesion,
+    botonRegistro,
+    butonGoogle
+  );
   return contenedorLogin;
 };
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -93,6 +93,7 @@ export const crearUsuarioConGoogle = () => {
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    
   });
 };
 
